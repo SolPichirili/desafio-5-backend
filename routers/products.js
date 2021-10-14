28f6f05d9713f74f6efe13fe3bafafc61eb3ac0d
Sol Pichirili <sol.pichirili@gmail.com>
2021-10-13 21:35:10 -0300

    correccion

--- routers/products.js
+++ routers/products.js
@@ -7,22 +7,16 @@ const productsContenedor = new Contenedor('./data/products.json');
 
 routerProducts.post('/', async (req, res) => {
     const newProduct = req.body;
-    if (newProduct.title === '' || newProduct.price === '' || newProduct.thumbnail === '') {
-        res.render('./pages/products-list.ejs', {
-            nuevo: newProduct,
-        });
-    } else {
+    if (newProduct.title !== '' || newProduct.price !== '' || newProduct.thumbnail !== '') {
         await productsContenedor.save(newProduct);
         res.redirect('/productos/list-productos');
     }
 });
 
 routerProducts.get('/list-productos', async (req, res) => {
-    const newProduct = req.body;
     const products = await productsContenedor.getAll();
     res.render('./pages/products-list.ejs', {
-        productos: products,
-        nuevo: newProduct
+        productos: products
     });
 });
 
